94ddcc260f4d880655135a53555ba1b8a51463b4
Thanakrit <ta200037@gmail.com>
2019-09-03 13:35:03 +0700

    first commit(fixed)

--- hello.c
+++ hello.c
@@ -1,4 +1,10 @@
-#include stdio.h
+#include "stdio.h"
+
+//git init ผูกเข้ากับ git 
+//git add hello.c
+//git commit -m "<comment>"
+//git remote add origin <web github>
+//git push -u origin <branch>
 
 main()
 {
