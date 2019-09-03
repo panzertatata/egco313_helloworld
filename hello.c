bafaeeaa399f804b8802c45ed9a2f4ad643b2176
Thanakrit <ta200037@gmail.com>
2019-09-03 14:17:37 +0700

    name

--- hello.c
+++ hello.c
@@ -5,8 +5,12 @@
 //git commit -m "<comment>"
 //git remote add origin <web github>
 //git push -u origin <branch>
+//git remote rm origin /delete origin
+//git remote -v /show all remote
+//git remote add <remote name> <http://github.com/<username>/<repository>>
 
 main()
 {
     printf("Hello world");
+    printf("Thanakrit");
 }
